a4044770add4e663fd20694a7cc5ac915475c1ed
Ty Petrochko <ty.petrochko@gmail.com>
2016-04-09 21:52:58 -0400

    Test not working... checking out one up

--- scripts/transfertest.fish
+++ scripts/transfertest.fish
@@ -3,7 +3,7 @@
 # edge 0 1 lossRate 0.2 delay 0 bw 10000 bt 1000 (Bps = ~900 - ~1500)
 # edge 0 1 lossRate 0.2 delay 200 bw 10000 bt 1000 (Bps = ~150 - ~250)
 # edge 0 1 lossRate 0.2 delay 200 bw 10000 bt 1000
-edge 0 1 lossRate 0.0 delay 0 bw 10000 bt 1000
+ edge 0 1 lossRate 0.0 delay 0 bw 10000 bt 1000
 time + 5
 # server port backlog [servint workint sz]
 0 server 21 2
